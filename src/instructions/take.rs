87f784457215f8f406836b293c24568a102009cb
MeerkatDev <lcampobasso@gmail.com>
2025-08-13 06:16:03 +0200

    make and take fmt

--- src/instructions/take.rs
+++ src/instructions/take.rs
@@ -3,13 +3,13 @@ use crate::{AssociatedTokenAccount, Escrow, MintInterface, ProgramAccount, Signe
 use pinocchio::account_info::AccountInfo;
 use pinocchio::instruction::Seed;
 use pinocchio::instruction::Signer;
+use pinocchio::msg;
 use pinocchio::program_error::ProgramError;
 use pinocchio::pubkey::create_program_address;
 use pinocchio::ProgramResult;
-use pinocchio_token::instructions::Transfer;
 use pinocchio_token::instructions::CloseAccount;
+use pinocchio_token::instructions::Transfer;
 use pinocchio_token::state::TokenAccount;
-use pinocchio::msg;
 
 pub struct TakeAccounts<'a> {
     pub taker: &'a AccountInfo,
